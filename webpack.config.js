78859b0084e219140d552179265177405e85d3e8
fuadmoin <fuadamoin@gmail.com>
2023-03-01 13:01:09 +0300

    Add todo list structure
    I added html css and javascript in src folder and used webpack to bundle them. And inside index.js I created the structure for the todo list. The display function will display based on the order of index.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+/* eslint-disable */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+  entry: './src/index.js',
+  output: {
+    filename: 'todo.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
