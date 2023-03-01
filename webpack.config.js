3eb64e1c872f5975022bbe59771aeedb2fc39e44
fuadmoin <fuadamoin@gmail.com>
2023-03-01 14:26:54 +0300

    Fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-
-  entry: './src/index.js',
-  output: {
-    filename: 'todo.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html'),
-    }),
-  ],
+  mode: 'development',
   module: {
     rules: [
       {
@@ -32,5 +20,21 @@ module.exports = {
         type: 'asset/resource',
       },
     ],
+  },
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  output: {
+    filename: 'todo.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
   },
 };
